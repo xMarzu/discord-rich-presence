d33cf0b652e7edcd7f9b4a4da4f47d463d1731fe
xMarzu <60571771+xMarzu@users.noreply.github.com>
2021-08-03 19:50:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 rpc = Presence("Client ID Here")
 rpc.connect()
-rpc.update(state="The Wording Below etails", details="Wordinsg Above State", large_image="Main Image Of Rich Presence", large_text="Text When Hovering Over Large Image", small_image="Small Image of Rich Presence",start=time.time())
+rpc.update(state="The Wording Below details", details="Wordings Above State", large_image="Main Image Of Rich Presence", large_text="Text When Hovering Over Large Image", small_image="Small Image of Rich Presence",start=time.time())
 
